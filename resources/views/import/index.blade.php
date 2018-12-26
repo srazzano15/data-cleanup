b89d3495bd8ecad79b1d9d4e3737dac93b8ff9c4
Steve Razzano <srazzano@allbound.com>
2018-12-26 10:38:25 -0700

    initial commit

--- resources/views/import/index.blade.php
+++ resources/views/import/index.blade.php
@@ -41,7 +41,7 @@
                                     <button type="submit" class="btn btn-primary">
                                         Parse CSV
                                     </button>
-                                </div>
+                                </div> 
                             </div>
                         </form>
                     </div>
